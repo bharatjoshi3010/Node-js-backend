d74faea64e6a9e759bbd8e13475de5e98eee71e7
unknown <bj20032003@gmail.com>
2026-01-02 18:50:51 +0530

    now we can change the files name

--- miniproject06/index.js
+++ miniproject06/index.js
@@ -18,6 +18,7 @@ app.get('/', function(req, res){
     })
 })
 
+//get used to retrive data from the server
 app.get('/file/:filesName', function(req, res){
     fs.readFile(`./files/${req.params.filesName}`, "utf-8", function(err, filedata){ // if we do not mention utf-8 here then it reads data in hexaDecimal format(buffer), utf-8 makes this buffer data to english characters
 
@@ -25,8 +26,22 @@ app.get('/file/:filesName', function(req, res){
     })
 })
 
+app.get('/edit/:filesName', function(req, res){
+    res.render('edit', {filename: req.params.filesName});
+})
+
 app.post('/create', function(req, res){                    //after clicking the submit button we come here and here it works create a file and write content on it.
+    //post -> used to send data to the server
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        //then it goes back to the home route(/)
+        //and '/' route re-render again and gives detail of the new file
+        res.redirect("/")
+    });
+})
+
+app.post('/edit', function(req, res){                    //after clicking the submit button we come here and here it changes the file name
+    //post -> used to send data to the server
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
         //then it goes back to the home route(/)
         //and '/' route re-render again and gives detail of the new file
         res.redirect("/")
