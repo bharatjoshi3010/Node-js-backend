675017aee63ff932a08f5ddee77678804facd510
unknown <bj20032003@gmail.com>
2026-01-03 15:55:53 +0530

    added some comments

--- mongoDb07/usermodel.js
+++ mongoDb07/usermodel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+// Connects to a local MongoDB database
+// mongopractice is the database name
+// If it doesn’t exist, MongoDB will create it automatically
 
+// A schema defines the structure of documents in a collection
 const userSchema = mongoose.Schema({
     name : String,
     username : String,
@@ -9,7 +13,7 @@ const userSchema = mongoose.Schema({
 })
 
 
-//We want to do CRUD opreation in any route, so for that we have to export the route first
+//We want to do CRUD opreation in any route, so for that we have to export the model first
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("user", userSchema); //MongoDB will create a collection named "users" (plural, lowercase), makes it plural
 // export is a propertie not a method
