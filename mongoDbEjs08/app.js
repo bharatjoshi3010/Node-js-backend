950d1c6258b87ded836ee46878237051eed548b7
unknown <bj20032003@gmail.com>
2026-01-05 15:32:32 +0530

    create user and delete user is working perfectly

--- mongoDbEjs08/app.js
+++ mongoDbEjs08/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -16,12 +17,28 @@ app.get('/', (req, res) => {
     res.render("index");            //it is rendering index.ejs
 })
 
-app.get('/read', (req, res) => {
-    res.render("read");            //it is rendering index.ejs
+app.get('/read', async(req, res) => {
+    let allusers = await userModel.find();
+    res.render("read", {users: allusers});            //it is rendering index.ejs
 })
 
-app.post('/create', (req, res) =>{
-    
+app.post('/create', async (req, res) => {
+    console.log("inside create route")
+    let {name, email, image} = req.body;    //destructuring from req.body
+
+    let createdUser = await userModel.create({
+        name,                    //its the destructuring sortcut name : name -> can be written as name only
+        email,
+        image
+    });
+
+    res.redirect("/read");
+
+})
+
+app.get('/delete/:id', async(req, res) => {
+    let allusers = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");           //it is rendering index.ejs
 })
 
 app.listen(3000);
